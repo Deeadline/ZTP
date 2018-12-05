7d75b83bc7381b5e281c2a736cf5f7611d98f3e9
Mikołaj Szymański <mikszym95@gmail.com>
2018-12-05 23:28:13 +0100

    Kosoraju Algorithm

---
+++ KosorajuAlgorithm/KosorajuAlgorithm/Graph.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KosorajuAlgorithm
+{
+    public class Graph
+    {
+        public int V { get; set; }
+        public LinkedList<int>[] AdjacencyLists { get; set; }
+
+        public Graph(int V)
+        {
+            this.V = V;
+            AdjacencyLists = new LinkedList<int>[V];
+            for (var i = 0; i < V; i++)
+            {
+                AdjacencyLists[i] = new LinkedList<int>();
+            }
+        }
+
+        public void AddEdge(int v, int w) => AdjacencyLists[v].AddLast(w);
+
+        /// <summary>
+        /// Find and print strongly connected components
+        /// </summary>
+        public void PrintScss()
+        {
+            //First DFS
+            var stack = new Stack<int>();
+            var visited = new bool[V];
+            // Fill vertices in stack according to finishing times
+            for (var i = 0; i < V; i++)
+            {
+                if (!visited[i])
+                    Fill(i, visited, stack);
+            }
+
+            //Second DFS
+            var transposedGraph = TransposeGraph();
+            visited = Enumerable.Repeat(false, V).ToArray();
+            while (stack.Count > 0)
+            {
+                var v = stack.Pop();
+                if (!visited[v])
+                {
+                    transposedGraph.Dfs(v, visited);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Function that returns transposed graph
+        /// </summary>
+        /// <returns>New transposed graph</returns>
+        private Graph TransposeGraph()
+        {
+            var transposed = new Graph(V);
+            for (var v = 0; v < V; v++)
+            {
+                var enumerator = AdjacencyLists[v].GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    transposed.AdjacencyLists[enumerator.Current].AddLast(v);
+                }
+            }
+            return transposed;
+        }
+
+        /// <summary>
+        ///  Fill stack with v. The top element of stack has the maximum finishing time.
+        /// </summary>
+        /// <param name="v">vertice</param>
+        /// <param name="visited">Array of visited v</param>
+        /// <param name="stack">Stack with maximum finishing time of each v</param>
+        private void Fill(int v, IList<bool> visited, Stack<int> stack)
+        {
+            visited[v] = true;
+            var enumerator = AdjacencyLists[v].GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (!visited[enumerator.Current])
+                    Fill(enumerator.Current, visited, stack);
+            }
+            stack.Push(v);
+        }
+
+        /// <summary>
+        /// DFS 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="visited"></param>
+        private void Dfs(int v, IList<bool> visited)
+        {
+            visited[v] = true;
+            Console.Write($"{v} ");
+
+            var enumerator = AdjacencyLists[v].GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (!visited[enumerator.Current])
+                    Dfs(enumerator.Current, visited);
+            };
+        }
+    }
+}
---
+++ KosorajuAlgorithm/KosorajuAlgorithm/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace KosorajuAlgorithm
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var graph = ReadFromFile();
+            graph.PrintScss();
+            Console.ReadKey();
+        }
+        private static Graph ReadFromFile()
+        {
+            Graph graph = null;
+            try
+            {
+                var path = Path.GetFullPath("..//..//..//Graph.txt");
+                using (var fileStream = File.OpenRead(path))
+                {
+                    using (var reader = new StreamReader(fileStream, Encoding.UTF8, true, 128))
+                    {
+                        string line;
+                        while (( line = reader.ReadLine() ) != null)
+                        {
+                            if (line.Length < 2)
+                            {
+                                graph = new Graph(int.Parse(line));
+                            }
+                            else
+                            {
+                                graph.AddEdge(int.Parse(line[0].ToString()), int.Parse(line[2].ToString()));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return graph;
+        }
+    }
+}
